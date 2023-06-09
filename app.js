b81c98751151ccd9805799ad42ed61bf105e55ed
validezeynalli <valide.zeynalli.03@gmail.com>
2023-06-09 17:17:56 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+let MainImg = document.getElementById("MainImg");
+let smallimg = document.getElementsByClassName("small-img");
+
+
+smallimg[0].onclick = function(){
+    MainImg.src = smallimg[0].src;
+}
+smallimg[1].onclick = function(){
+    MainImg.src = smallimg[1].src;
+}
+smallimg[2].onclick = function(){
+    MainImg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function(){
+    MainImg.src = smallimg[3].src;
+}
+
+
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar ){
+    bar.addEventListener('click', () =>{
+        nav.classList.add('active')
+    })
+}
+if (close ){
+    bar.addEventListener('click', () =>{
+        nav.classList.remove('active')
+    })
+}
